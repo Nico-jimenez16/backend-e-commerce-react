55d1b64ac41b5783275b2c0aad43a68fedda1385
Nico-jimenez16 <jimeneznicolas520@gmail.com>
2022-11-22 05:43:24 -0300

    last change cors *

--- build/index.js
+++ build/index.js
@@ -4,11 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+let cors = require("cors");
 // ! importamos rutas
 const users_1 = __importDefault(require("./routes/users"));
 const products_1 = __importDefault(require("./routes/products"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use(cors());
 const PORT = 8080;
 app.get('/', (_req, res) => {
     res.send('La pagina de Inicio');
